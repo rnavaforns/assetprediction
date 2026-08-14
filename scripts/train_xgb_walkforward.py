85b83a8fa76080e17c94600ddd08dcb4378c049a
Ral <raulnavarro174@gmail.com>
2026-08-14 08:47:11 +0200

    Fix imports

--- scripts/train_xgb_walkforward.py
+++ scripts/train_xgb_walkforward.py
@@ -4,7 +4,6 @@ import numpy as np
 import xgboost as xgb
 from datetime import datetime
 import wandb
-import matplotlib.subplots as plt_sub
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import TimeSeriesSplit
